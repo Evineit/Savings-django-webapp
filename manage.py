88c8e080d13e7b26a9d3d2bd95c87196b78b9cf4
Kevin G. Rodriguez <46513310+Evineit@users.noreply.github.com>
2021-01-08 20:05:54 -0600

    Add: Postgres and docker support (#6)
    * Add: Postgres and Docker support
    
    * Fix: added missing entrypoint script

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from pathlib import Path
+import dotenv
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project5.settings')
+    dotenv.load_dotenv(Path('.env.example'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
